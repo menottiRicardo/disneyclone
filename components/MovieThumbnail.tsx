04846ae1edd99388e8320b7fff97997744aecdee
menottiRicardo <ricardomenotti31@gmail.com>
2021-11-20 18:35:23 -0500

    Update MovieThumbnail.tsx

--- components/MovieThumbnail.tsx
+++ components/MovieThumbnail.tsx
@@ -25,7 +25,7 @@ const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
     <div
       className="flex min-w-[250px] min-h-[170px] md:min-w-[200px] md:min-h-[150px] 2xl:min-w-[300px] 2xl:min-h-[150px]  rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-0  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300 w-52 h-32 sm:w-64 sm:h-36 2xl:w-80 2xl:h-48"
-      onClick={() => router.push(`/movie/${movie.id}`)}
+    //   onClick={() => router.push(`/movie/${movie.id}`)}
     >
       <Image
         src={
